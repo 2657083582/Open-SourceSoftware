d196bfa4bf7a923e04529edb30bd122baa1ce381
lsw <2657083582@qq.com>
2023-03-29 15:33:11 +0800

    创建项目

--- build-LayerPaint-C_C_Qt_6_3_1-Debug/CMakeFiles/LayerPaint.dir/LayerPaint_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/LayerPaint.dir/LayerPaint_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
- /root/study/开源软件杯/Layerpaint/Open-SourceSoftware/build-LayerPaint-C_C_Qt_6_3_1-Debug/LayerPaint_autogen/EWIEGA46WW/qrc_qml.cpp \
- /usr/include/stdc-predef.h
